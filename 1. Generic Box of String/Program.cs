379deea7079bd86ec17a2edd24ceac43aa207662
bulaf88 <bulaf88@abv.bg>
2019-02-10 17:37:40 +0200

    Add project files.

---
+++ 1. Generic Box of String/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1._Generic_Box_of_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
